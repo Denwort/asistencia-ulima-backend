f324263097901702c338dbe3a89f5e5e1e493f3d
L33202 <L33202@lab.aca>
2024-06-11 08:45:51 -0500

    boolean registro para la sesion

--- db/migrations/008_create_sesiones.rb
+++ db/migrations/008_create_sesiones.rb
@@ -2,6 +2,7 @@ Sequel.migration do
   change do
     create_table(:sesiones) do
       primary_key :id
+      TrueClass :registro, null: false, default: false
       DateTime :fechaInicio, null: false
       DateTime :fechaFin, null: false
       foreign_key :seccion_id, :secciones, null: false, on_delete: :cascade
